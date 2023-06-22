0969e0ef53cbb3a0f343464477b750d1a2c20e87
Aleksey Lisun <insuline.ru@gmail.com>
2023-06-22 14:50:29 +0300

    feat: add get smart account and check delegate util

---
+++ src/utils/get-smart-accounts.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import type { Address } from 'viem';
+
+import { SMART_MARGIN_FACTORY_ABI } from '../abi';
+import { initClients } from '../config';
+import { SMART_MARGIN_FACTORY } from '../constants/address';
+
+dotenv.config();
+
+async function getSmartAccounts(address: Address) {
+	const { publicClient } = initClients();
+	const chainId = process.env.CHAIN_ID!;
+
+	const targetAccounts = await publicClient.readContract({
+		abi: SMART_MARGIN_FACTORY_ABI,
+		address: SMART_MARGIN_FACTORY[chainId],
+		functionName: 'getAccountsOwnedBy',
+		args: [address],
+	});
+
+	return targetAccounts;
+}
+
+export { getSmartAccounts };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,3 @@
 export * from './parse-execute-log';
+export * from './check-delegate';
+export * from './get-smart-accounts';
