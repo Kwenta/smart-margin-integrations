abc0a8b058df972247c9cbbbb94e833b650bddfc
Aleksey Lisun <insuline.ru@gmail.com>
2023-06-27 18:23:17 +0300

    chore: add sUSD addresses

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -71,3 +71,8 @@ export const PERPS_V2_MARKET_DATA: Record<string, Address> = {
 	420: '0xcE2dC389fc8Be231beECED1D900881e38596d7b2',
 	10: '0x340B5d664834113735730Ad4aFb3760219Ad9112',
 };
+
+export const sUSD_SYNTH_ADDRESS: Record<string, Address> = {
+	420: '0xebaeaad9236615542844adc5c149f86c36ad1136',
+	10: '0x57ab1ec28d129707052df4df418d58a2d46d5f51',
+};
