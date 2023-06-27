4c10611f79e4a4b750006e91b5991827cf11567f
Aleksey Lisun <insuline.ru@gmail.com>
2023-06-27 14:06:12 +0300

    feat: use parser

--- src/copytrading.ts
+++ src/copytrading.ts
@@ -4,7 +4,8 @@ import { decodeFunctionData, formatUnits, isAddress } from 'viem';
 import { SMART_MARGIN_ACCOUNT_ABI } from './abi';
 import { initClients } from './config';
 import { checkDelegate, getIdleMargin, getSmartAccounts } from './utils/prepare';
-import { parseExecuteData } from './utils/trade';
+import { getPositions } from './utils/prepare/get-positions';
+import { parseExecuteData, parseOperationDetails } from './utils/trade';
 
 const { publicClient, walletClient } = initClients();
 
@@ -36,13 +37,22 @@ async function main() {
 	}
 
 	const targetAccounts = await getSmartAccounts(targetWallet);
+	const formattedTargetAccounts = targetAccounts.map((account) => account.toLowerCase());
 
 	if (targetAccounts.length === 0) {
 		console.error('Target KWENTA accounts not found');
 		return;
 	}
 
-	const formattedTargetAccounts = targetAccounts.map((account) => account.toLowerCase());
+	const targetPositionsResponse = await Promise.all(
+		targetAccounts.map(async (account) => {
+			return getPositions(account);
+		})
+	);
+	const targetPositions = targetPositionsResponse
+		.flat()
+		.filter((position) => position.position.size !== 0n);
+	const repeaterPositions = await getPositions(repeaterWallet);
 
 	const repeaterBalance = await publicClient.readContract({
 		abi: SMART_MARGIN_ACCOUNT_ABI,
@@ -50,7 +60,7 @@ async function main() {
 		functionName: 'freeMargin',
 	});
 
-	const { idleInMarkets, positions } = await getIdleMargin(repeaterWallet);
+	const { idleInMarkets } = await getIdleMargin(repeaterWallet);
 
 	const totalBalance = idleInMarkets + repeaterBalance;
 	const totalBalanceInUSD = formatUnits(totalBalance, 18);
@@ -78,24 +88,26 @@ async function main() {
 						});
 
 						if (functionName === 'execute') {
-							parseExecuteData(args);
-
-							try {
-								// TODO: Add sUSD approve check here
-								console.log('Sending transaction to repeater wallet');
-								const hash = await walletClient.sendTransaction({
-									to: repeaterWallet,
-									data: input,
-									gas: (gas * 110n) / 100n,
-								});
-								console.log('Transaction sent', hash);
-								await publicClient.waitForTransactionReceipt({ hash });
-							} catch (e) {
-								console.error(e);
-							}
+							const operations = parseExecuteData(args);
+							const operationDetails = parseOperationDetails(operations, targetPositions);
+							console.log({ operationDetails });
+							// try {
+							// 	// TODO: Add sUSD approve check here
+							// 	console.log('Sending transaction to repeater wallet');
+							// 	const hash = await walletClient.sendTransaction({
+							// 		to: repeaterWallet,
+							// 		data: input,
+							// 		gas: (gas * 110n) / 100n,
+							// 	});
+							// 	console.log('Transaction sent', hash);
+							// 	await publicClient.waitForTransactionReceipt({ hash });
+							// } catch (e) {
+							// 	console.error(e);
+							// }
 						}
 					}
 				} catch (e) {
+					console.error(e);
 					continue;
 				}
 			}
