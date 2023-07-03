5debe88fb1690d3a2371e63a2915d498c930b802
Aleksey Lisun <insuline.ru@gmail.com>
2023-07-03 17:43:43 +0300

    fix: remove useless `async`

--- src/utils/prepare/get-idle-margin.ts
+++ src/utils/prepare/get-idle-margin.ts
@@ -5,8 +5,7 @@ interface IdlePosition {
 	idleMargin: bigint;
 }
 
-// TODO: Use SDK to get margin from markets
-async function getIdleMargin(positions: PositionDetail[]) {
+function getIdleMargin(positions: PositionDetail[]) {
 	const positionsWithIdleMargin = positions.filter(
 		(p) => p.position?.size === 0n && p.remainingMargin > 0n
 	);
