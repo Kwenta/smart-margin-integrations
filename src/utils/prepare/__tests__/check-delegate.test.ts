08a856ddc08ced16af6e6607ce4bd0469fd96182
Aleksey Lisun <insuline.ru@gmail.com>
2023-07-07 15:28:02 +0300

    feat: check delegate tests

---
+++ src/utils/prepare/__tests__/check-delegate.test.ts
@@ -0,0 +1,145 @@
+import { isAddressEqual } from 'viem';
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
+
+import { SMART_MARGIN_ACCOUNT_ABI } from '../../../abi';
+import { initClients } from '../../../config';
+import { checkDelegate } from '../check-delegate';
+
+jest.mock('../../../config', () => ({
+	initClients: jest.fn(),
+}));
+
+jest.mock('viem', () => ({
+	isAddressEqual: jest.fn(),
+}));
+
+describe('checkDelegate', () => {
+	it('should return true when the executor is owner', async () => {
+		const randomWallet = privateKeyToAccount(generatePrivateKey());
+
+		const repeaterWallet = randomWallet.address;
+		const executorAddress = randomWallet.address;
+
+		const mockedClients = {
+			publicClient: {
+				multicall: jest.fn().mockResolvedValue([{ result: null }, { result: executorAddress }]),
+			},
+			walletClient: {
+				account: {
+					address: executorAddress,
+				},
+			},
+		};
+
+		(initClients as jest.Mock).mockReturnValue(mockedClients);
+		(isAddressEqual as jest.Mock).mockReturnValue(true);
+
+		const result = await checkDelegate({ repeaterWallet });
+
+		expect(result).toBe(true);
+		expect(isAddressEqual).toHaveBeenCalledWith(executorAddress, executorAddress);
+		expect(mockedClients.publicClient.multicall).toHaveBeenCalledWith({
+			contracts: [
+				{
+					address: repeaterWallet,
+					abi: SMART_MARGIN_ACCOUNT_ABI,
+					functionName: 'delegates',
+					args: [executorAddress],
+				},
+				{
+					address: repeaterWallet,
+					abi: SMART_MARGIN_ACCOUNT_ABI,
+					functionName: 'owner',
+				},
+			],
+		});
+	});
+
+	it('should return true when the executor is a delegate', async () => {
+		const randomWallet = privateKeyToAccount(generatePrivateKey());
+		const secondRandomWallet = privateKeyToAccount(generatePrivateKey());
+
+		const repeaterWallet = randomWallet.address;
+		const executorAddress = randomWallet.address;
+		const otherAddress = secondRandomWallet.address;
+
+		const mockedClients = {
+			publicClient: {
+				multicall: jest
+					.fn()
+					.mockResolvedValue([{ result: executorAddress }, { result: otherAddress }]),
+			},
+			walletClient: {
+				account: {
+					address: executorAddress,
+				},
+			},
+		};
+
+		(initClients as jest.Mock).mockReturnValue(mockedClients);
+		(isAddressEqual as jest.Mock).mockReturnValue(false);
+
+		const result = await checkDelegate({ repeaterWallet });
+
+		expect(result).toBe(true);
+		expect(isAddressEqual).toHaveBeenCalledWith(otherAddress, executorAddress);
+		expect(mockedClients.publicClient.multicall).toHaveBeenCalledWith({
+			contracts: [
+				{
+					address: repeaterWallet,
+					abi: SMART_MARGIN_ACCOUNT_ABI,
+					functionName: 'delegates',
+					args: [executorAddress],
+				},
+				{
+					address: repeaterWallet,
+					abi: SMART_MARGIN_ACCOUNT_ABI,
+					functionName: 'owner',
+				},
+			],
+		});
+	});
+
+	it('should return false when the executor is neither a delegate nor an owner', async () => {
+		const randomWallet = privateKeyToAccount(generatePrivateKey());
+		const secondRandomWallet = privateKeyToAccount(generatePrivateKey());
+
+		const repeaterWallet = randomWallet.address;
+		const executorAddress = randomWallet.address;
+		const otherAddress = secondRandomWallet.address;
+
+		const mockedClients = {
+			publicClient: {
+				multicall: jest.fn().mockResolvedValue([{ result: null }, { result: otherAddress }]),
+			},
+			walletClient: {
+				account: {
+					address: executorAddress,
+				},
+			},
+		};
+
+		(initClients as jest.Mock).mockReturnValue(mockedClients);
+		(isAddressEqual as jest.Mock).mockReturnValue(false);
+
+		const result = await checkDelegate({ repeaterWallet });
+
+		expect(result).toBe(false);
+		expect(isAddressEqual).toHaveBeenCalledWith(otherAddress, executorAddress);
+		expect(mockedClients.publicClient.multicall).toHaveBeenCalledWith({
+			contracts: [
+				{
+					address: repeaterWallet,
+					abi: SMART_MARGIN_ACCOUNT_ABI,
+					functionName: 'delegates',
+					args: [executorAddress],
+				},
+				{
+					address: repeaterWallet,
+					abi: SMART_MARGIN_ACCOUNT_ABI,
+					functionName: 'owner',
+				},
+			],
+		});
+	});
+});
